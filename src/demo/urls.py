942029179bb93177997e67346c96cff507757762
difegam <difegam3@gmail.com>
2026-02-14 22:53:37 +0100

    Remove demo app files including admin, apps, migrations, models, tests, urls, and views to clean up the project structure.

--- src/demo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import home
-
-app_name = 'demo'
-
-urlpatterns = [
-    path('', home, name='home'),
-]
--- src/demo/views.py
+++
@@ -1,8 +0,0 @@
-from django.http.request import HttpRequest
-from django.http.response import HttpResponse
-from django.shortcuts import render
-
-
-# Remove
-def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'index.html')
