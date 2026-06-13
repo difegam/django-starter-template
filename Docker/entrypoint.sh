742917d88c485d1b4b56d8e4550c9b642f157a5a
difegam <difegam3@gmail.com>
2026-06-13 23:55:50 +0200

    feat: improve Docker entrypoint with Docker secrets support

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,21 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+file_env 'DJANGO_SUPERUSER_PASSWORD'
 
 python src/manage.py migrate --noinput
 python src/manage.py collectstatic --noinput
 
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
-    if [ -n "$DJANGO_SUPERUSER_PASSWORD_FILE" ] && [ -f "$DJANGO_SUPERUSER_PASSWORD_FILE" ]; then
-        su_password=$(cat "$DJANGO_SUPERUSER_PASSWORD_FILE")
-        export DJANGO_SUPERUSER_PASSWORD="$su_password"
-    fi
-
-    if [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
-        python src/manage.py createsuperuser \
-            --noinput \
-            --username "$DJANGO_SUPERUSER_USERNAME" \
-            --email "$DJANGO_SUPERUSER_EMAIL" 2>/dev/null || true
-    fi
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && \
+   [ -n "$DJANGO_SUPERUSER_EMAIL" ] && \
+   [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python src/manage.py createsuperuser \
+        --noinput \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" 2>/dev/null || true
 fi
 
 exec "$@"
